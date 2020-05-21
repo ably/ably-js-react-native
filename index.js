1fabd4a165a55ce1c00cbd97f2380f538bfd3661
steelbrain <i@steelbrain.me>
2020-05-22 03:35:46 +0500

    :new: Add configurePush for Push notifications

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 import Ably from 'ably';
+import { Platform } from 'react-native';
+
+export function configurePush({ token }) {
+  if (Platform.OS === 'android') {
+    Ably.platform.push._pushDetails = {
+      transportType: 'fcm',
+      registrationToken: token,
+    };
+    return;
+  }
+  if (Platform.OS === 'ios') {
+    Ably.platform.push._pushDetails = {
+      transportType: 'apns',
+      deviceToken: token,
+    };
+    return;
+  }
+
+  throw new Error(`Unsupported OS: ${Platform.OS}`);
+}
 
 export default { ...Ably };
